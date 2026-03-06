94ad10e94b3e8eae07ecc40f32ebc440c31d1a98
388 sittinon <sittinonpv05@gmail.com>
2026-03-06 13:17:19 +0700

    Update project for new repository assignment

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -24,6 +24,10 @@ async function main() {
             title: 'Cloud Deployment',
             description: 'Deploy Backend to Render and Frontend to Netlify',
         },
+        {
+            title: 'นาย ปราวิต ประภาวรารัตน์',
+            description: 'รหัส 6604101408',
+        },
     ];
 
     for (const t of initialTasks) {
